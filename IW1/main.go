90901b35a633303bd81d961fd62106bad4d48f81
unknown <sanyapridava@mail.ru>
2024-05-29 23:37:50 +0300

    Create a tree node and constructor, initializer childs

--- IW1/main.go
+++ IW1/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+type TreeNode struct {
+	value       int
+	left_child  *TreeNode
+	right_child *TreeNode
+}
+
+func NewTreeNode(value int) *TreeNode {
+
+	return &TreeNode{value: value, left_child: nil, right_child: nil}
+}
+
+func (tree_node *TreeNode) setRightChild(child_node *TreeNode) {
+	tree_node.right_child = child_node
+}
+
+func (tree_node *TreeNode) setLeftChild(child_node *TreeNode) {
+	tree_node.left_child = child_node
+}
 
 func main() {
-	fmt.Println("Hello")
+
 }
