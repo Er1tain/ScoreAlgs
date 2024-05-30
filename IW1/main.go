0121ce82e3a8cfb1262e9e2826aea4a844d74c3a
unknown <sanyapridava@mail.ru>
2024-05-30 14:01:01 +0300

    New method

--- IW1/main.go
+++ IW1/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type TreeNode struct {
 	value       int
@@ -8,7 +10,7 @@ type TreeNode struct {
 	right_child *TreeNode
 }
 
-func NewTreeNode(value int) *TreeNode {
+func CreateNodeTree(value int) *TreeNode {
 
 	return &TreeNode{value: value, left_child: nil, right_child: nil}
 }
@@ -19,6 +21,25 @@ func (tree_node *TreeNode) setRightChild(child_node *TreeNode) {
 
 func (tree_node *TreeNode) setLeftChild(child_node *TreeNode) {
 	tree_node.left_child = child_node
+}
+
+func addNode(root *TreeNode, value int) {
+	if root == nil {
+		return
+	}
+	if value <= root.value {
+		if root.left_child == nil {
+			root.setLeftChild(CreateNodeTree(value))
+		} else {
+			addNode(root.left_child, value)
+		}
+	} else {
+		if root.right_child == nil {
+			root.setRightChild(CreateNodeTree(value))
+		} else {
+			addNode(root.right_child, value)
+		}
+	}
 }
 
 func (tree_node *TreeNode) print() {
@@ -33,12 +54,35 @@ func (tree_node *TreeNode) getRightChild() *TreeNode {
 	return tree_node.right_child
 }
 
+func createBinarryTree(root *TreeNode, values []int) {
+	for _, value := range values {
+		addNode(root, value)
+	}
+}
+
+func countNodesWithOneChild(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	count := 0
+	if (root.getLeftChild() != nil && root.getRightChild() == nil) || (root.getLeftChild() == nil && root.getRightChild() != nil) {
+		count++
+	}
+
+	count += countNodesWithOneChild(root.getLeftChild())
+	count += countNodesWithOneChild(root.getRightChild())
+
+	return count
+}
+
 func main() {
+	values := []int{5, -23, -7, 7, 3, 1, -5, -6, 12, -12}
+	root := CreateNodeTree(values[0])
+	values = values[1:]
+	createBinarryTree(root, values)
 
-	root := NewTreeNode(8)
-	root.setLeftChild(NewTreeNode(5))
-	root.setRightChild(NewTreeNode(13))
-
-	root.getRightChild().print()
+	count_onechild_nodes := countNodesWithOneChild(root)
+	fmt.Println(count_onechild_nodes)
 
 }
